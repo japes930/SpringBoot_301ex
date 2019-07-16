9b6b5fb8ddeca11769934fa86dbfb21c8c42002b
japes930 <jpvillaflores9@gmail.com>
2019-07-16 16:07:39 -0400

    first commit

---
+++ src/main/java/com/example/springboot_301ex/Springboot301exApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_301ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot301exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot301exApplication.class, args);
+	}
+
+}
